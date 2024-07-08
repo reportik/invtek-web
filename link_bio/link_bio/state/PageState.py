a85f76be5885c482f3c7143a8cc31e3ff2c499a2
reportik <79289765+reportik@users.noreply.github.com>
2024-07-08 06:14:17 -0600

    xx

---
+++ link_bio/link_bio/state/PageState.py
@@ -0,0 +1,38 @@
+import asyncio
+import reflex as rx
+import link_bio.utils as utils
+from link_bio.api.api import live, featured, schedule
+from link_bio.model.Featured import Featured
+from link_bio.model.Live import Live
+
+USER = "mouredev"
+
+class PageState(rx.State):
+    timezone = ""
+    live_status = Live(live=False, title="")
+    next_live = ""
+    featured_info: list[Featured]
+
+    async def check_live(self):
+        self.live_status = await live(USER)
+
+    def check_schedule(self):
+        if self.timezone == "":
+            return rx.call_script(
+                utils.LOCAL_TIMEZONE_SCRIPT,
+                PageState.update_timezone_sync
+            )
+        else:
+            self.update_timezone(self.timezone)
+
+    @staticmethod
+    def update_timezone_sync(timezone: str):
+        # Usa asyncio.run para ejecutar la coroutine
+        asyncio.run(PageState().update_timezone(timezone))   
+        
+    def update_timezone(self, timezone: str):
+        self.timezone = timezone
+        self.next_live = utils.next_date(schedule(), self.timezone) 
+
+    async def featured_links(self):
+        self.featured_info = await featured()
