b2dc11e4d9488ce4102bf3413a4e4722567c9415
reportik <79289765+reportik@users.noreply.github.com>
2024-07-08 14:23:21 -0600

    Update remote_build.sh

--- link_bio/remote_build.sh
+++ link_bio/remote_build.sh
@@ -1,11 +1,8 @@
 cd link_bio
 python -m venv .venv
 source .venv/bin/activate
-pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
+reflex run --env prod --loglevel debug
 deactivate
